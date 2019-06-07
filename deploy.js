0b0d9006c26d1ac5d0df460d0afe15ef82e7e87c
kongw <kongwela75@gmail.com>
2019-06-07 16:22:50 -0700

    updated deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '100.24.42.52',
+      host: '18.208.147.230',
       username: 'ubuntu',
       privateKey: 'jk-key.pem'
     })
